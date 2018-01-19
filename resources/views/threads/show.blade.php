f52975e35c2cb5b9de2fe30b0a3535152ea9c5e0
Mario Mello <mario@dindigital.com>
2018-01-18 22:04:29 -0200

    Aula 16 - Meta Details and Pagination

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <a href="#">{{ $thread->creator->name }}</a> posted:
@@ -13,38 +13,46 @@
                         {{$thread->body}}
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    @foreach($thread->replies as $reply)
+                @foreach($replies as $reply)
+                    <div class="panel panel-default">
                         @include('threads.reply')
-                    @endforeach
+                    </div>
+                @endforeach
+
+                {{  $replies->links() }}
+
+                @if(auth()->check())
+                    <form method="post" action="{{$thread->path() . '/replies'}}">
+                        {{ csrf_field() }}
+                        <textarea name="body" class="form-control"
+                                  placeholder="Have something to say?"></textarea>
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+                @else
+                    <p class="text-center">You need <a href="{{route('login')}}">sign in</a> to participate.</p>
+                @endif
+
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>
+                            This thread was published
+                            {{ $thread->created_at->diffForHumans() }} by
+                            <a href="#">{{ $thread->creator->name }}</a> and currently has
+                            {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                        </p>
+                    </div>
                 </div>
             </div>
+
         </div>
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-            @if(auth()->check())
-                <form method="post" action="{{$thread->path() . '/replies'}}">
-                    {{ csrf_field() }}
-                    <textarea name="body" class="form-control"
-                    placeholder="Have something to say?"></textarea>
-                    <button type="submit" class="btn btn-default">Post</button>
-                </form>
-            @else
-                <p class="text-center">You need <a href="{{route('login')}}">sign in</a> to participate.</p>
-            @endif
-            </div>
-        </div>
-
-        <br />
-        <br />
-        <br />
-        <br />
+        <br/>
+        <br/>
+        <br/>
 
     </div>
 @endsection
